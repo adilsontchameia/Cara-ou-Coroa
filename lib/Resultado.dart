22f0e489ced43cfe865ba68e18ac1faea55c210f
adilsonchameia <adilsonkchameia@gmail.com>
2021-03-19 11:36:11 +0100

    Criacao da interface resultado

---
+++ lib/Resultado.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class Resultado extends StatefulWidget {
+  @override
+  _ResultadoState createState() => _ResultadoState();
+}
+
+class _ResultadoState extends State<Resultado> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      //Utiliza-se preimeiro o prefixo 0xff - Color(0xff ),
+      //Com opacidade - Color.fromRGBO(r, g, b, opacity) // (255, 204, 128, 0.8),
+      backgroundColor: Color(0xff61bd86),
+      body: Container(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Image.asset("images/logo.png"),
+            GestureDetector(
+              onTap: _exibirResultado,
+              child: Image.asset("images/botao_jogar.png"),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
