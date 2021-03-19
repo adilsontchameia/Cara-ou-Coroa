d343bfc4357b91db5f2c94e9d004b2deafe37a69
adilsonchameia <adilsonkchameia@gmail.com>
2021-03-19 11:45:53 +0100

    Usando navigator.pop na interface resultado

--- lib/Resultado.dart
+++ lib/Resultado.dart
@@ -7,10 +7,6 @@ class Resultado extends StatefulWidget {
 }
 
 class _ResultadoState extends State<Resultado> {
-  void _botaoVoltar() {
-    Navigator.push(context, MaterialPageRoute(builder: (context) => Jogar()));
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -24,7 +20,10 @@ class _ResultadoState extends State<Resultado> {
           children: [
             Image.asset("images/moeda_cara.png"),
             GestureDetector(
-              onTap: _botaoVoltar,
+              //Fecha interface atual e leva na anterior
+              onTap: () {
+                Navigator.pop(context);
+              },
               child: Image.asset("images/botao_voltar.png"),
             )
           ],
