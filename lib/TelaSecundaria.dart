de56eef4b51307f93002583e9a6d1937f92bebfd
adilsonchameia <adilsonkchameia@gmail.com>
2021-03-19 11:23:09 +0100

    Criacao do body jogar.

---
+++ lib/TelaSecundaria.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class Jogar extends StatefulWidget {
+  @override
+  _JogarState createState() => _JogarState();
+}
+
+class _JogarState extends State<Jogar> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.green,
+      body: Container(),
+    );
+  }
+}
